a03873bf588260f8561cabc9a161ce44b1c960ac
Vladimir <vovabelous@mail.ru>
2024-11-11 22:02:04 +0300

    refactoring

--- RequestsTest/test/test_pokemons.py
+++ RequestsTest/test/test_pokemons.py
@@ -6,14 +6,15 @@ TOKEN = 'ce3965733d527e392ccbfd3519a2429e'
 HEADER = {'Content-Type' : 'application/json', 'trainer_token':TOKEN}
 TRANIER_ID = 17443
 TRAINER_NAME = 'Python'
+
 def test_status_code():
     response = requests.get(url=f'{URL}/pokemons',params= {'trainer_id' : TRANIER_ID})
-    assert response.status_code == 200
+    assert response.status_code == 200, 'Unexpected status code'
 
 def test_part_of_respons():
     response_get = requests.get(url=f'{URL}/pokemons',params= {'trainer_id' : TRANIER_ID})
-    assert response_get.json()["data"][0]["name"] == 'Vorsa'
+    assert response_get.json()["data"][0]["name"] == 'Vorsa', 'Unexpected pokemone name'
 
 def test_trainer_name():
     trainer_get = requests.get(url=f'{URL}/trainers',params= {'trainer_id' : TRANIER_ID})
-    assert trainer_get.json()["data"][0]["trainer_name"] == 'Python'
+    assert trainer_get.json()["data"][0]["trainer_name"] == 'Python', 'Unexpected trainer name'
